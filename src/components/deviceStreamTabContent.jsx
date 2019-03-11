8368624d015f289d01b5a64b325fa44db3d87290
Artemy Magarin <magar.art@yandex.ru>
2019-03-12 00:13:05 +0300

    added streaming tab

--- src/components/deviceStreamTabContent.jsx
+++ src/components/deviceStreamTabContent.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import { apiUrl } from '../config';
+import '../styles/switch.css';
+import '../styles/style.css';
 
-const DeviceStreamTabContent = ({snapshot_url, stream_url}) => {
-	return (
-		<p>{`${apiUrl}${snapshot_url}, ${apiUrl}${stream_url}`}</p>
-	)
+
+export default class DeviceStreamTabContent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tab: 'snapshot'
+		}
+	}
+
+	togglePicture = () => {
+		if (this.state.tab === 'snapshot') {
+			this.setState({tab: 'stream'});
+		} else {
+			this.setState({tab: 'snapshot'});
+		}
+	}
+
+	render() {
+		let url = apiUrl + (this.state.tab === 'snapshot' ? this.props.snapshot_url : this.props.stream_url);
+		url = url + (~url.indexOf('?')?'&':'?') + `t=${Date.now()}`; 
+		
+		const toggler = (
+			<div className="switch-wrapper">
+				Show snapshot
+		        <label className="switch">
+		            <input type="checkbox" checked={this.state.tab === 'stream'} onClick={this.togglePicture}/>
+		            <span className="slider"></span>
+		        </label>
+		        Show Stream
+		    </div>
+		)
+		const image = (
+			<img src={url} className="stream-image"/>
+		)
+		return (
+			<>
+				{toggler}
+				{image}
+			</>
+		);
+	}
 }
-
-export default DeviceStreamTabContent;
