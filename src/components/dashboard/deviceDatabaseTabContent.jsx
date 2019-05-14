bcb587478fbf09f856ad42e4d5aa258440a64543
Artemy Magarin <magar.art@yandex.ru>
2019-05-15 02:51:32 +0300

    reports table fixes

--- src/components/dashboard/deviceDatabaseTabContent.jsx
+++ src/components/dashboard/deviceDatabaseTabContent.jsx
@@ -17,35 +17,30 @@ class DeviceDatabaseTabContent extends React.Component {
      )
     }
 
-    handleDownload = (uri) => {
-      console.log(uri);
-    }
-
     render() {
         const tableItems = this.state.reports && this.state.reports.map((item, id) => (
             <tr>
               <th className="col-1" scope="row">{id+1}</th>
-              <td className="col-4">{ item.device.ip }</td>))
+              <td className="col-4">{ item.device && item.device.ip }</td>))
               <td className="col-4">{ item.created }</td>))
               <td className="col-3">
-                <button className="btn btn-primary" onClick={() => this.handleDownload(item.url)}>
+                <a className="btn btn-primary" target={"_blank"} href={`${apiUrl}/${item.url}`}>
                   Download
-                </button>
+                </a>
               </td>
             </tr>))
     return (
         <div className="card-body">
             <h5 className="card-title">Previous reports:</h5>
             {this.state.isFetched ? (
-            <table className="table table-bordered table-responsive-md">
+            this.state.reports.length ? (<table className="table table-bordered table-responsive-md">
               <tbody>
                 {tableItems}
               </tbody>
-            </table>):
+            </table>) : "No avaliable reports"):
             (<div class="spinner-border" role="status">
               <span class="sr-only">Loading...</span>
             </div>)}
-
         </div>
     )
 }
